27145c319bf210672b7f3ee15508745e58c8f3a1
fehu <lord.fehu@gmail.com>
2015-04-29 16:57:59 -0500

    Spinner reactions fixed;

--- src/main/scala/feh/dsl/swing/swing/Spinner.scala
+++ src/main/scala/feh/dsl/swing/swing/Spinner.scala
@@ -2,12 +2,18 @@ package feh.dsl.swing.swing
 
 import javax.swing.{SpinnerModel, JSpinner, JComponent}
 
-import scala.swing.Component
+import scala.swing.event.ValueChanged
+import scala.swing.{Publisher, Component}
 
-class Spinner[T](val model: SpinnerModel) extends Component{
+class Spinner[T](val model: SpinnerModel) extends Component with Publisher{
   override lazy val peer: JSpinner = new JSpinner(model)
 
   def value = peer.getValue.asInstanceOf[T]
   def value_=(v: T) { peer.setValue(v) }
 
+  peer.addChangeListener(new javax.swing.event.ChangeListener {
+    def stateChanged(e: javax.swing.event.ChangeEvent) {
+      publish(new ValueChanged(Spinner.this))
+    }
+  })
 }
